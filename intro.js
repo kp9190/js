e26d37cef0db926fe91c61a5fe10df6dd4f625ab
kp <kenilpatel204@gmail.com>
2025-09-24 18:18:36 +0530

    intro

---
+++ intro.js
@@ -0,0 +1,26 @@
+
+
+// console.log("patel");
+
+
+// console.error("This is Error Mess");
+    
+
+// console.warn("this is warning Mess");
+
+// var num= [11,12,13];
+
+// console.info(num);
+
+
+// console.table(num);
+
+
+document.write("<h1>This is JS sesstion</h1>");
+document.write("<h1>This is my First Sesstion</h1>");
+
+
+// Task 1 : print your Bio Data using the documnet.write
+
+// task 2 : print information about your fvrt color name any 5 lisng using console 
+
