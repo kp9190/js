ac85e42be0b547fad79fd13d5af514cd30d2cba0
kenil patel <kenilpatel204@gmail.com>
2025-10-14 18:09:46 +0530

    string1

--- string/one.js
+++ string/one.js
@@ -1,17 +1,22 @@
 // string 
 
 // var str = "Rajesh";
+// var str1 = 'rajesh';
 
-// console.log(str);
-// console.log(typeof str);
+// console.log(str1);
+// console.log(typeof str1);
 
 // console.log(str);// index
 
 
 // string methods
 
-var str = "Rajesh";
-var str1 = "My Name is Rajesh"
+var str = "Rajesh"; // h
+var str1 = "My Name is Rajesh"; // h
+
+// console.log(str);
+// console.log(str1);
+
 
 // 1. length
 
@@ -20,17 +25,17 @@ var str1 = "My Name is Rajesh"
 
 // 2. charAt
 
-// console.log(str.charAt(0)); // index value
-// console.log(str1.charAt(2)); // index value
+// console.log(str.charAt(5)); // index value
+// console.log(str1.charAt()); // index value
 
 // 3. chaCodeAt
 
 // console.log(str.charAt(1)); // index value
-// console.log(str.charCodeAt(1)); // index value ascii code
+// console.log(str.charCodeAt(0)); // index value ascii code
 
 // 4. endsWith
 
-// console.log(str.endsWith("s")); // return true / false  
+// console.log(str.endsWith('s')); // return true / false  
 // last char  
 
 // 5. startsWith 
@@ -40,10 +45,68 @@ var str1 = "My Name is Rajesh"
 
 // 6. indexOf
 
-// console.log(str.indexOf("r")); // to search string index
+// console.log(str.indexOf("j")); // to search string index
 // not in string -1 return 
 
-// console.log(str1.indexOf("a")); // point the index of first char
+// console.log(str.indexOf("a")); // point the index of first char
+
+// 7. lastIndexof
+
+// console.log(str.lastIndexOf("a"));
+
+// 8. replace
+
+// console.log(str.replace("a","A"));
+// console.log(str1.replace("a","A"));
+
+// 9. replace All
+
+// console.log(str1.replaceAll("My","A"));
+
+// 10. splite
+
+// var demo = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim suscipit quisquam est praesentium maxime, adipisci totam ipsa esse repellendus. Cupiditate, quaerat quo voluptatum hic illo necessitatibus iusto doloribus cum aut!";
+
+// console.log(demo.split("."));
+
+// 11.toLowerCase  && toUpperCase
+
+// console.log(str1.toLowerCase());
+// console.log(str1.toUpperCase());
+
+// 12. trim
+// var demo = "        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim suscipit quisquam    est praesentium maxime, adipisci totam ipsa esse repellendus. Cupiditate, quaerat quo voluptatum hic illo necessitatibus iusto doloribus cum aut!            "
+
+// console.log(demo.trim());
+
+// 13. trimEnd && trimStart
+
+// console.log(str1);
+// console.log(str1.trimEnd());
+// console.log(str1.trimStart());
+
+// 14. slice
+
+// var demo = str1.slice(3,7); // slice(include, exclude)
+// console.log(demo);// index 
+// console.log(typeof demo);
+
+
+
+// 15. substring
+// var demo = str1.substring(0,5);
+// console.log();// substring(include, exclude)
+// console.log(typeof demo);
+
+
+// 16. concat()
+
+// var demo =  str.concat(" ",str1);
+// console.log(demo);
+
+// 17. str[]
+
+// console.log(str[5]);
 
 
 
