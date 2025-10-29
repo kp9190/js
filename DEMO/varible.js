227190859d37f8bba0dd323883cee486bf144583
kenil patel <kenilpatel204@gmail.com>
2025-10-29 17:48:26 +0530

    if-else

---
+++ DEMO/varible.js
@@ -0,0 +1,81 @@
+// console.log();
+// Hello world program.
+// 1. write a program to prting "hello world " to the console. 
+// console.log("Hello world");
+
+// 2. modify the "hello world " program to include your name is the greeting.
+// var fname= "kneil";
+// console.log(fname);
+
+// Declare variable using var.
+//  1. Declare a variable using `var` and assign it a value. Reassign the value and print it.
+// var value1; 
+// value1 =12;
+// console.log(value1);
+// value1 =14;
+// console.log(value1);
+
+// 2. Declare two `var` variables and swap their values.
+// var v1 =12;
+// var v2 =14;
+
+// console.log("Before Swap : ",v1,v2);
+
+// var temp = v1;
+// v1 =v2;
+// v2 = temp;
+// console.log("After Swap : ",v1,v2);
+
+
+// More about variable.
+
+//  1. Declare variables using `var`, `let`, and `const`. Assign each a different value and print them.
+// var v1 =12;
+// let v2 =13;
+// const v3 =14;
+
+// console.log(v1);
+// console.log(v2);
+// console.log(v3);
+
+// 2. Try reassigning a `const` variable and observe the error.
+// v3 = 15;
+
+// 3. Declare a `let` variable inside a block and print it outside the block.
+// {
+//     let v1 =13;
+//     console.log(v1);
+// }
+// console.log(v1);
+
+///String/
+// What is a String : Collection of Multiple Char. 
+//              String not an Array but it's similler an Array.
+// 
+//  using Double Quotation
+//  length      12345
+     var Str = "Royal";
+     //index    01234
+
+ console.log(Str);
+
+ // using Single Quotation
+ var Str2 = 'Technosoft';
+ console.log(Str2);
+ 
+ // using Back-tick
+ var Str3 = `p ltd`;
+ console.log(Str3);  
+ console.log(Str[2]);
+ 
+// String Indexing.
+
+// 1. Create a string and access each character using its index.
+// var str ="Royal";
+// console.log(str);
+// console.log(str[1]);
+
+// Useful string methods.
+// Template Strings.
+// Null, undefined, BigInt, typeof.
+// Booleans and Comparison Operator.
---
+++ if-else/if.js
@@ -0,0 +1,76 @@
+// if  statement : 
+
+//  adult ?
+
+// var age = 10;
+// if(agr >=18){
+//     console.log("You are Adult.");
+    
+// }
+
+
+// if-else Statement
+
+// var age = 21;
+// if(age >=18){
+//     console.log("You are Adult.");   
+// }else{
+//     console.log("You are Not Adult.");   
+// }
+
+// else if 
+
+// var num =0;
+
+// if(num >0 ){
+//     console.log("Num  +");
+// }else if(num < 0){
+//     console.log("num -");
+// }else if(num == 0){
+//     console.log("num is Zero");
+// }
+
+//Nesed if-Else
+
+// var num1=21;
+// var num2 =20;
+// var num3 =14;
+
+// if(num1 >num2){
+//     if(num1>num3){
+//         console.log("Num1 is Big");
+//     }else{
+//         console.log("Num3 is Big");
+//     }
+// }else{
+//     if(num2>num3){
+//         console.log("Num2 is Big");
+//     }else{
+//         console.log("Num3 is Big");
+//     }
+// }
+
+
+// ==> number Positive/Negative/Zero Using  if else
+// 
+// ==>  Using else if 
+//      A = 90 + 
+//      B = 80 +
+//      C = 70 +
+//      D = 60 +
+//      50< fail
+// 
+// ==>  Find the Leap Year using nested if-else
+
+// ### If Else Statement
+// 1. Write a function that checks if a number is positive, negative, or zero.
+// 2. Use `if-else` to check if a string contains more than 10 characters.
+// 3. Write a function that checks if a person is eligible to vote based on their age.
+// 4. Use `if-else` to print whether a number is a multiple of 3.
+// 5. Write a function that checks if a value is `null` or `undefined`.
+// 6. Use `if-else` to check if a number is even or odd.
+// 7. Write a function that takes a temperature in Celsius and prints a message if it is above 30 degrees.
+// 8. Use `if-else` to check if a number is within a specific range.
+// 9. Write a function that checks if a password is strong (contains at least 8 characters, including numbers and special characters).
+// 10. Use `if-else` to print a message based on the value of a boolean variable.
+
