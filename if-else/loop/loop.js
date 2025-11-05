35b85d79331415acfd62173498173bdf9b638699
kenil patel <kenilpatel204@gmail.com>
2025-11-05 18:00:32 +0530

    loop

---
+++ if-else/loop/loop.js
@@ -0,0 +1,39 @@
+// while loop
+
+// while (expression) {
+//     // statement
+// }
+
+var num=1;
+
+// while (num < 10) {
+//     console.log(num);
+//     num++;
+// }
+
+// for loop
+// for(i=1; i<10; i++){
+//     console.log(i);
+// }
+
+
+
+// do...while
+// do{
+//     console.log(num);
+//     num--;
+// }while(num > 0);
+
+
+// breack;
+while (1) {
+    if(num == 10)
+    {
+        break;
+    }else{
+        console.log(num);
+        num++;
+    }
+}
+
+
