3a0c5d895ec48f1ef504d0d0270cd95219e0e1ad
kenil patel <kenilpatel204@gmail.com>
2025-11-06 18:27:13 +0530

    loop patte

---
+++ loop/loop.js
@@ -0,0 +1,178 @@
+// while loop
+
+// while (expression) {
+//     // statement
+// }
+
+var num=1;
+
+// while (num < 10) {
+//     console.log(num);
+//     num++;
+// }
+
+// for loop
+// for(i=1; i<10; i++){
+//     console.log(i);
+// }
+
+
+
+// do...while
+// do{
+//     console.log(num);
+//     num--;
+// }while(num > 0);
+
+
+// breack;
+// while (1) {
+//     if(num == 10)
+//     {
+//         break;
+//     }else{
+//         console.log(num);
+//         num++;
+//     }
+// }
+
+
+/*
+*
+* * 
+* * * 
+* * * *
+*/
+// console.log(typeof null);
+
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null==0);
+
+// let num1 = "2";
+// let num2 = 1;
+
+
+// console.log(typeof num1);
+// console.log(typeof num2);
+
+// console.log(num1 + num2);// 2 1 21
+// console.log(num2 + num1);// 12
+// console.log(num2 + 3 + num1); // 1 + 3 +'2' => 4 + '2' => 42
+
+
+
+// let str1 = "hello";
+// let str2 = " kenil";
+// let str3 = str1 + str2;
+// console.log(str1 + str2 + str1 + str2 );
+// console.log(str1 , str2 , str3);// only two concet
+// console.log(str3);
+
+// console.log("hii" + str1);
+// console.log("hii " , str1);
+// console.log("hii " , str1, str2 + str3);
+
+/*
+*
+* * 
+* * * 
+* * * *
+*/
+
+// let str="";
+
+// for(i=0; i<10; i++){
+//     str = "";
+
+//     for(j=0; j<=i; j++){// j =2
+//         str= str + " *"; // str =" * * *"
+//     }
+    
+//     console.log(str);    
+// }
+
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+*/
+
+// let str ="";
+// let k;
+
+// for(let i =0 ; i<5; i++){
+//     str="";
+//     k=1;
+//     for(let j =0; j<=i; j++){
+//         str= str + " " +k;
+//         k++;
+//     }
+//     console.log(str);
+// }
+
+let k=65;
+for(let i=0; i<5; i++){
+    console.log( String.fromCharCode(k) + " ");
+    k++;
+}
+
+
+
+
+/*
+1) n=4
+      *
+    * * *
+  * * * * *
+* * * * * * *
+
+2) n=4
+
+      *
+    * * *
+  * * * * *
+* * * * * * *
+  * * * * *
+    * * *
+      *
+
+
+3) n=4
+
+1
+2 1
+3 2 1
+4 3 2 1
+
+
+4) n=4 
+
+* * * * * * *
+  * * * * *
+    * * *
+      *  
+*/
+
+/*
+1. Write a for loop to print numbers from 1 to 10.
+2. Create a for loop that prints the elements of an array. (var Arr = [1,2,3,4,5,6,7,7];)
+3. Write a for loop to print the multiplication table of a given number.
+4. Create a for loop that calculates the factorial of a given number.
+5. Use a for loop to iterate over an array of objects and print each object's properties.
+6. Write a for loop to print the first 10 odd numbers.
+7. Create a for loop that prints the elements of a matrix (2D array)./////////
+8. Write a for loop that finds the sum of all elements in an array.
+9. Use a for loop to find the maximum number in an array.
+10. Create a for loop that prints the reverse of a string.
+
+
+*/
+
+
+
+
+
+
+
